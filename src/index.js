24ee502a75738404dda7b61863a6211e48f98b52
SillyScribe95 <sillyscribe95@gmail.com>
2021-08-22 10:37:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,34 +13,37 @@ export * from "./functions/arrayFuncs";
 export * from "./functions/dictFuncs";
 export * from "./functions/lengthFuncs";
 export * from "./functions/linkFuncs";
-export * from "./functions/bediaFuncs";
 export * from "./functions/globalFuncs";
 export * from "./functions/pageFuncs";
-// export * from "./functions/timeFuncs";
 export * from "./functions/stringFuncs";
-export * from "./functions/bookFuncs";
-export * from "./functions/scrapeFuncs";
-export * from "./functions/playerFuncs";
 export * from "./functions/emojiFuncs";
-export * from "./functions/notionFuncs";
-export * from "./functions/userFuncs";
-export * from "./functions/backendFuncs";
 export * from "./functions/copyFuncs";
 export * from "./functions/chromeFuncs";
 export * from "./functions/scrapeFuncs";
 
+// export * from "./functions/bediaFuncs";
+// export * from "./functions/bookFuncs";
+// export * from "./functions/scrapeFuncs";
+// export * from "./functions/playerFuncs";
+// 1extra
+// export * from "./functions/notionFuncs";
+// export * from "./functions/userFuncs";
+// export * from "./functions/backendFuncs";
+
 // 1backend
-export * from "./functions/firebaseFuncs";
+// export * from "./functions/firebaseFuncs";
+
+// export * from "./functions/timeFuncs";
 
 // 1data 1model
-export * from "./functions/modelFuncs";
-export * from "./functions/mediaFuncs";
-export * from "./functions/highlightFuncs";
-export * from "./functions/playlistFuncs";
+// export * from "./functions/modelFuncs";
+// export * from "./functions/mediaFuncs";
+// export * from "./functions/highlightFuncs";
+// export * from "./functions/playlistFuncs";
 
 //
 // 1user
-export * from "./functions/user/SaveUser";
+// export * from "./functions/user/SaveUser";
 
 // 1const
 export * from "./consts/constMain";
@@ -49,13 +52,13 @@ export * from "./consts/constLink";
 export * from "./consts/constStyle";
 
 // 1test
-export * from "./extra/testTopic";
-export * from "./extra/testPlaylist";
-export * from "./extra/testMedia";
-export * from "./extra/testHighlight";
-export * from "./extra/testUser";
-export * from "./extra/testNotion";
-export * from "./extra/testExtra";
+// export * from "./extra/testTopic";
+// export * from "./extra/testPlaylist";
+// export * from "./extra/testMedia";
+// export * from "./extra/testHighlight";
+// export * from "./extra/testUser";
+// export * from "./extra/testNotion";
+// export * from "./extra/testExtra";
 // export * from "./extra/testHighlights";
 
 // 1user
